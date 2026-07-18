89e1113d6cc93b92b8814ab7ad68b2c178da25d5
brsvrn <brsvrn@gmail.com>
2026-07-18 05:08:13 +0300

    Create manifest.js

---
+++ app/manifest.js
@@ -0,0 +1,48 @@
+export default function manifest() {
+  return {
+    name: "Vertice Stok",
+    short_name: "Vertice Stok",
+
+    description:
+      "QR ve barkod destekli stok, sayım ve envanter yönetim sistemi.",
+
+    start_url: "/",
+
+    scope: "/",
+
+    display: "standalone",
+
+    orientation: "portrait",
+
+    background_color: "#030712",
+
+    theme_color: "#030712",
+
+    categories: [
+      "business",
+      "productivity",
+      "utilities",
+    ],
+
+    icons: [
+      {
+        src: "/icons/icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/icons/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/icons/icon-maskable-512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+    ],
+  };
+}
